d7a99cfaa6fd7083a59e05ef5faf0efca984f071
selinaageier <selina.geier@iu-study.org>
2024-11-13 11:43:19 +0100

    Hamburger-Menü, Anpassungen Header

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ document.addEventListener("mousemove", function(event) {
     cursor.style.left = event.pageX + 'px';
     cursor.style.top = event.pageY + 'px';
 });
+
+
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('nav').classList.toggle('active');
+});
