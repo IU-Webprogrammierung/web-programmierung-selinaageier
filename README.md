308b0a040a9fad04f94b1ab0e48f0656dd7704a9
selinaageier <selina.geier@iu-study.org>
2024-11-14 18:16:37 +0100

    Anpassungen Bilder und Texte in CSS

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # web-programmierung
+
+## Projekt lokal ausführen
+1. Extension "Live Server" installieren
+2. Projekt öffnen
+3. Live Server starten (im Browser unter `http://127.0.0.1:5500/index.html` sichtbar)
--- script.js
+++ script.js
@@ -8,13 +8,22 @@ document.addEventListener("mousemove", function(event) {
 });
 
 // Hamburger-Menü
-document.querySelector('.menu-toggle').addEventListener('click', function() {
-    document.querySelector('nav').classList.toggle('active');
-});
+window.onload = function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    if (menuToggle) {
+        menuToggle.addEventListener('click', function() {
+            document.querySelector('nav').classList.toggle('active');
+        });
+    } else {
+        console.error('Element .menu-toggle wurde nicht gefunden!');
+    }
+};
 
 // Navigation
+
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('DOMContentLoaded event wurde ausgelöst');
+    console.log('DOMContentLoaded event wurde ausgelöst!');
+
     const nav = document.querySelector('nav');
     if (!nav) {
         console.error('Das <nav>-Element wurde nicht gefunden!');
@@ -23,12 +32,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const links = document.querySelectorAll('nav a');
     console.log('Gefundene Links:', links);
+
     const currentPath = window.location.pathname.replace(/\/+$/, '');
+    console.log('Aktueller Pfad:', currentPath);
 
     links.forEach(link => {
-        const linkHref = '/' + link.getAttribute('href').replace(/^\/+/, '').replace(/\/+$/, '');
+        let linkHref = link.getAttribute('href').replace(/^\/+/, '');
+        linkHref = `/${linkHref}`;
+        
         console.log('Link Href:', linkHref);
-        if (currentPath.includes(linkHref)) {
+
+        if (currentPath === linkHref) {
+            console.log(`Aktiv: ${linkHref}`);
             link.classList.add('active');
         }
     });
